93381b867aa5dba7be44bcbe384f5aeee4b7adb2
Ikkyy <msndoikky@hotmail.com>
2019-08-12 12:23:28 -0300

    Conclusão

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'
+import MostraVoltas from './MostraVoltas'
+import MostraTempo from './MostraTempo'
+import Button from './Button'
 
 function App() {
+  const [numVoltas, setNumVoltas] = useState(1)
+  const [rodando, setRodando] = useState(false)
+  const [tempo, setTempo] = useState(0)
+
+  useEffect(() => {
+    let timer = null
+    if (rodando) {
+      timer = setInterval(() => {
+        setTempo(anterior => anterior + 1)
+      }, 1000)
+    }
+    return () => {
+      if (timer) {
+        clearInterval(timer)
+      }
+    }
+  }, [rodando])
+
+  const toggleRodando = () => {
+    setRodando(!rodando)
+  }
+
+
+  const incrementar = () => {
+    setNumVoltas(numVoltas + 1)
+  }
+  const decrementar = () => {
+    if (numVoltas > 0) {
+      setNumVoltas(numVoltas - 1)
+    }
+  }
+
+  const reseta = () => {
+    setNumVoltas(0)
+    setTempo(0)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MostraVoltas voltas={numVoltas} />
+      <Button className="maior" texto='+' onClick={incrementar} />
+      <Button className="maior" texto='-' onClick={decrementar} />
+      {
+        numVoltas > 0 &&
+        <MostraTempo tempo={Math.round(tempo / numVoltas)} />
+      }
+      <Button onClick={toggleRodando} texto={ rodando ? "Pausar" : "Iniciar"} />
+      <Button onClick={reseta} texto='Reiniciar' />
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Button = (props) => <button {...props}>{props.texto}</button>
+
+export default Button
